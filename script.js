ba091ed86b3b6172fb457cdbd332c7daa3cfe371
Jordan Stammer <s.jordanstammer@gmail.com>
2023-10-18 22:13:58 -0400

    Add initial display functionality
    The display will now concatanate numbers pressed in realtime.

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
-add = (operandA, operandB) => operandA + operandB;
-subtract = (operandA, operandB) => operandA - operandB;
-multiply = (operandA, operandB) => operandA * operandB;
-divide = (operandA, operandB) => operandA / operandB;
-
-function operate(operandA, operandB, operator){
-    return operator(operandA, operandB);
+let calculator = {
+    display: null,
 }
 
+let screen = document.querySelector('#screen')
+screen.textContent = calculator.display
+
 let numberKeys = document.querySelectorAll('.number')
-let operatorKeys = document.querySelectorAll('.operator')
-let equalKey = document.querySelector('#equals-key')
-let clearKey = document.querySelector('#ce-key')
+
+function pressNumber(newNumber){
+    if (calculator.display === null){
+        if(newNumber == 0){
+            return;
+        }
+        calculator.display = newNumber;
+        screen.textContent = calculator.display;
+        return;
+    }
+    calculator.display = `${calculator.display}` + `${newNumber}`;
+    screen.textContent = calculator.display
+}
+
+numberKeys.forEach((element) => element.addEventListener('click', () => pressNumber(element.textContent)))
 
 
-numberKeys.forEach((element) => element.addEventListener('click', () =>{
-    alert(`I am a ${element.textContent} key`)
-}))
+// add = (operandA, operandB) => operandA + operandB;
+// subtract = (operandA, operandB) => operandA - operandB;
+// multiply = (operandA, operandB) => operandA * operandB;
+// divide = (operandA, operandB) => operandA / operandB;
 
-operatorKeys.forEach((element) => element.addEventListener('click', () =>{
-    alert(`I am a ${element.textContent} key`)
-}))
+// function operate(operandA, operandB, operator){
+//     return operator(operandA, operandB);
+// }
 
-equalKey.addEventListener('click', () => alert(`I am a ${equalKey.textContent} key`))
-clearKey.addEventListener('click', () => alert(`I am a ${clearKey.textContent} key`))
+
+// let operatorKeys = document.querySelectorAll('.operator')
+// let equalKey = document.querySelector('#equals-key')
+// let clearKey = document.querySelector('#ce-key')
+
+
+
+
+
+
+
+
+
+// operatorKeys.forEach((element) => element.addEventListener('click', () =>{
+//     alert(`I am a ${element.textContent} key`)
+// }));
+
+// equalKey.addEventListener('click', () => alert(`I am a ${equalKey.textContent} key`));
+// clearKey.addEventListener('click', () => alert(`I am a ${clearKey.textContent} key`));
