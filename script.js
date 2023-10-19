0e458933c6dec42292f43384e30154ff4fa397e8
Jordan Stammer <s.jordanstammer@gmail.com>
2023-10-18 20:46:51 -0400

    Add Number Event Listeners
    Event Listners added, currenty not functional with operators,
    but numbers buttons will alert their text content

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ subtract = (operandA, operandB) => operandA - operandB;
 multiply = (operandA, operandB) => operandA * operandB;
 divide = (operandA, operandB) => operandA / operandB;
 
-
-console.log('Should be 10', add(5,5));
-console.log('Should be 10', subtract(15,5));
-console.log('Should be 10', multiply(2,5));
-console.log('should be 10', divide(100,10));
-
 function operate(operandA, operandB, operator){
     return operator(operandA, operandB);
 }
 
-console.log(operate(5,12,subtract))
+let numberKeys = document.querySelectorAll('.number')
+
+numberKeys.forEach((element) => element.addEventListener('click', () =>{
+    alert(element.textContent)
+}))
